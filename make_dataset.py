6d8b44f8d089c9f700b4950efc38c5ab64c3ab33
Yang Liu <foamliu@yeah.net>
2018-06-05 18:15:38 +0800

    update

--- make_dataset.py
+++ make_dataset.py
@@ -40,12 +40,12 @@ def compute_color_prior(X_ab, size=64, do_plot=False):
             ax.set_ylim([-110, 110])
 
     npts, c, h, w = X_ab.shape
-    X_a = np.ravel(X_ab[:, 0, :, :])
-    X_b = np.ravel(X_ab[:, 1, :, :])
+    X_a = np.ravel(X_ab[:, :, :, 0])
+    X_b = np.ravel(X_ab[:, :, :, 1])
     X_ab = np.vstack((X_a, X_b)).T
 
     if do_plot:
-        plt.hist2d(X_ab[:, 0], X_ab[:, 1], bins=100, norm=LogNorm())
+        plt.hist2d(X_ab[:, :, :, 0], X_ab[:, :, :, 1], bins=100, norm=LogNorm())
         plt.xlim([-110, 110])
         plt.ylim([-110, 110])
         plt.colorbar()
