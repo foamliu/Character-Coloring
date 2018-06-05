d878dd3917a9aada1900f4d32e08da96ab79983f
Yang Liu <foamliu@yeah.net>
2018-06-05 19:00:14 +0800

    update

--- make_dataset.py
+++ make_dataset.py
@@ -6,6 +6,8 @@ import matplotlib.pylab as plt
 import numpy as np
 import sklearn.neighbors as nn
 from matplotlib.colors import LogNorm
+from scipy.interpolate import interp1d
+from scipy.signal import gaussian, convolve
 from skimage import color
 
 
@@ -71,10 +73,61 @@ def compute_color_prior(X_ab, size=64, do_plot=False):
     prior_prob = prior_prob / (1.0 * np.sum(prior_prob))
 
     # Save
-    np.save(os.path.join(data_dir, "CelebA_%s_prior_prob.npy" % size), prior_prob)
+    np.save(os.path.join(data_dir, "prior_prob.npy"), prior_prob)
 
     if do_plot:
         plt.hist(prior_prob, bins=100)
+        plt.yscale("log")
+        plt.show()
+
+
+def smooth_color_prior(size=64, sigma=5, do_plot=False):
+    prior_prob = np.load(os.path.join(data_dir, "CelebA_%s_prior_prob.npy" % size))
+    # add an epsilon to prior prob to avoid 0 vakues and possible NaN
+    prior_prob += 1E-3 * np.min(prior_prob)
+    # renormalize
+    prior_prob = prior_prob / (1.0 * np.sum(prior_prob))
+
+    # Smooth with gaussian
+    f = interp1d(np.arange(prior_prob.shape[0]), prior_prob)
+    xx = np.linspace(0, prior_prob.shape[0] - 1, 1000)
+    yy = f(xx)
+    window = gaussian(2000, sigma)  # 2000 pts in the window, sigma=5
+    smoothed = convolve(yy, window / window.sum(), mode='same')
+    fout = interp1d(xx, smoothed)
+    prior_prob_smoothed = np.array([fout(i) for i in range(prior_prob.shape[0])])
+    prior_prob_smoothed = prior_prob_smoothed / np.sum(prior_prob_smoothed)
+
+    # Save
+    file_name = os.path.join(data_dir, "CelebA_%s_prior_prob_smoothed.npy" % size)
+    np.save(file_name, prior_prob_smoothed)
+
+    if do_plot:
+        plt.plot(prior_prob)
+        plt.plot(prior_prob_smoothed, "g--")
+        plt.plot(xx, smoothed, "r-")
+        plt.yscale("log")
+        plt.show()
+
+
+def compute_prior_factor(size=64, gamma=0.5, alpha=1, do_plot=False):
+    file_name = os.path.join(data_dir, "CelebA_%s_prior_prob_smoothed.npy" % size)
+    prior_prob_smoothed = np.load(file_name)
+
+    u = np.ones_like(prior_prob_smoothed)
+    u = u / np.sum(1.0 * u)
+
+    prior_factor = (1 - gamma) * prior_prob_smoothed + gamma * u
+    prior_factor = np.power(prior_factor, -alpha)
+
+    # renormalize
+    prior_factor = prior_factor / (np.sum(prior_factor * prior_prob_smoothed))
+
+    file_name = os.path.join(data_dir, "CelebA_%s_prior_factor.npy" % size)
+    np.save(file_name, prior_factor)
+
+    if do_plot:
+        plt.plot(prior_factor)
         plt.yscale("log")
         plt.show()
 
@@ -84,4 +137,6 @@ if __name__ == '__main__':
     do_plot = True
 
     X_ab = load_data()
-    compute_color_prior(X_ab, size=64, do_plot=True)
+    compute_color_prior(X_ab, do_plot=True)
+    smooth_color_prior(do_plot=True)
+    compute_prior_factor(do_plot=True)
